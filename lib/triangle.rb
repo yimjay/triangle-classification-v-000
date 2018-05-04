7264cde8da502dd7e6e679df6b46f7df3aa91fab
Ji Yim <gsooyim@gmail.com>
2018-05-04 23:53:25 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   attr_accessor :a, :b, :c
   
   def initialize(attributes)
-    attributes.each {|key, value| self.send{("#key}=", value)}}
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
   
   def kind
