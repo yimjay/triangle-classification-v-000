b6953e3528e13c53ac611fe19b59928d1cd7da9c
Ji Yim <gsooyim@gmail.com>
2018-05-05 00:00:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
   
   def kind
-    if a > 0 && b > 0 && c > 0 && ((a + b > c) || (a + c > b) || (b + c > a))
+    if a > 0 && b > 0 && c > 0 && ((a + b > c))
       if a == b && b == c
         :equilateral
       elsif a == b || a == c || b == c
